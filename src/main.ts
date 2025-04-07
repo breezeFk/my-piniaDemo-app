e53225b499ad4da59807ac25b0792ea836c2cb65
breezeFk <18872326126@163.com>
2025-04-07 11:26:54 +0800

    第一次提交

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import { createPinia } from 'pinia'
+const app = createApp(App)
+const store=createPinia()
+app.use(store)
+app.mount('#app')
